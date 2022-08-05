43b4b406e22cc5b52a7eaa630e48a8af2ed4e975
gunnerDgd <ifstudiomail@naver.com>
2022-08-05 12:12:26 +0900

    CMAKE Directory Updated.

---
+++ source/shared_lib/init.c
@@ -0,0 +1,9 @@
+#include <filesystem/defines/export/export.h>
+
+synapse_filesystem_dll
+    int
+        synapse_initialize_sysmod() { return 0; }
+
+synapse_filesystem_dll
+    void
+        synapse_cleanup_sysmod() {}
